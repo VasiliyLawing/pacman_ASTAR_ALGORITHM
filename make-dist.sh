7cc556dfa0d984a2977a7947ed8fec7c61b228d8
dek <d.kischukov@gmail.com>
2019-07-19 16:38:06 -0400

    gradle.properties now in git - script

--- make-dist.sh
+++ make-dist.sh
@@ -4,7 +4,7 @@ prj_name="prg-b1-examples"
 
 rm -Rf dist
 mkdir -p dist/${prj_name}
-cp -R src gradle gradlew* build.gradle dist/${prj_name}
+cp -R src gradle gradlew* build.gradle gradle.properties dist/${prj_name}
 cd dist
 tar -czf ${prj_name}.tar.gz ${prj_name}
 cd ..
