3d2de397c2bccd5443013789d3c2db185d73b614
dek <d.kischukov@gmail.com>
2019-12-14 21:58:54 -0500

    .

---
+++ src/main/kotlin/level2/unit2/tasks/task2.kt
@@ -0,0 +1,27 @@
+package level2.unit2.tasks
+
+import com.anysolo.toyGraphics.*
+
+
+private const val amountOfRows = 10
+private const val amountOfColumns = 15
+private const val blockWidth = 75
+private const val blockHeight = 25
+private const val blockDistance = 8
+
+
+// Write functions drawBlock and calcBlockPosition here
+
+
+fun main() {
+    val wnd = Window(1280, 720)
+    val g = Graphics(wnd)
+
+/*
+Uncomment this when you wrote missing functions and check how it works.
+    for (row in 0 until  amountOfRows)
+        for (column in 0 until  amountOfColumns)
+            drawBlock(g, calcBlockPosition(column, row))
+
+ */
+}
