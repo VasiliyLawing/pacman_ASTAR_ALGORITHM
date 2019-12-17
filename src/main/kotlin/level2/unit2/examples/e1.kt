5437bac11ddd8aa1aae08554a22c23716cb5fb52
dlong <dlong@anysolo.com>
2019-12-17 09:29:44 -0500

    l2.u2 e1 and e2.

--- src/main/kotlin/level2/unit2/examples/e1.kt
+++ src/main/kotlin/level2/unit2/examples/e1.kt
@@ -2,12 +2,12 @@ package level2.unit2.examples
 
 
 // Create our own data type/class containing x,y coordinates in form of two Ints
-data class Point(val x: Int, val y: Int)
+data class Pos(val x: Int, val y: Int)
 
 
 fun main() {
     // Create a variable of Pos type
-    val playerPos = Point(300,600)
+    val playerPos = Pos(300,600)
 
     // Print it just for debugging porpoise.
     println("#1")
@@ -18,7 +18,12 @@ fun main() {
     println("${playerPos.x}, ${playerPos.y}")
 
     // You can have as many variables of that type as you want
-    val enemyPos = Point(100, 0)
+    val enemyPos = Pos(100, 0)
     println("#3")
     println("playerPos=$playerPos, enemyPos=$enemyPos")
+
+    val playerPos2 = Pos(300,600)
+    val playerPos3 = Pos(300,601)
+    println("playerPos == playerPos2: ${playerPos == playerPos2}")
+    println("playerPos2 == playerPos3: ${playerPos2 == playerPos3}")
 }
