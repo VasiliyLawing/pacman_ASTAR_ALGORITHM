a9b64c40e1a5975ec179b229c649348c7b755900
dek <d.kischukov@gmail.com>
2020-02-15 10:11:47 -0500

    .

--- src/main/kotlin/level2/unit2/tasks/task1.kt
+++ src/main/kotlin/level2/unit2/tasks/task1.kt
@@ -10,14 +10,7 @@ private const val blockHeight = 25
 private const val blockDistance = 8
 
 
-fun drawBlock(g: Graphics, x: Int, y: Int) {
-    g.color = Pal16.brightCyan
-    g.drawRect(x, y, prgb1.solutions.level2.unit2.task1.blockWidth, prgb1.solutions.level2.unit2.task1.blockHeight, fill = true)
-
-    g.setStrokeWidth(5)
-    g.color = Pal16.black
-    g.drawRect(x, y, prgb1.solutions.level2.unit2.task1.blockWidth, prgb1.solutions.level2.unit2.task1.blockHeight)
-}
+// Write function drawBlock here
 
 fun main() {
     val wnd = Window(1280, 720)
