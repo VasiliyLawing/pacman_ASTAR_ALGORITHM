8817e7ca073d9f5e959df99594059f4cac03e890
dek <d.kischukov@gmail.com>
2022-05-02 10:59:24 -0400

    .

---
+++ src/main/kotlin/level3/e7.kt
@@ -0,0 +1,17 @@
+package level3
+
+
+
+fun main() {
+    // You can use Pair data class when you need a simple two value class
+    val nameAndAge = Pair("John Smith", 11)
+    println(nameAndAge)
+    println(nameAndAge.first)
+    println(nameAndAge.second)
+
+    // Exactly the same but using "to" operator
+    val nameAndAge2 = "John Smith" to 11
+    println(nameAndAge2.first)
+    println(nameAndAge2.second)
+
+}
