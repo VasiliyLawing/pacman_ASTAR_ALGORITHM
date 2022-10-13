3d0a74e2881c57039bed8d60c82b8232685054d8
dek <d.kischukov@gmail.com>
2022-10-13 19:35:44 -0400

    .

--- src/main/kotlin/level2v3/unit1/examples/e4.kt
+++ src/main/kotlin/level2v3/unit1/examples/e4.kt
@@ -5,7 +5,7 @@
 package level2v3.unit1.examples
 
 
-// A function cam return a result. You have to declare the type of result in function header. You see it below.
+// A function can return a result. You have to declare the type of result in function header. You see it below.
 // Function declaration does not return the result, just declares it.
 fun numberInPowerOf(n: Int, power: Int): Int {
     var result = 1
