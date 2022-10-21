466da12f0f37dc97238c7e73485e618694024316
dek <d.kischukov@gmail.com>
2022-10-21 11:07:49 -0400

    .

---
+++ src/main/kotlin/level2v3/unit5/examples/e4.kt
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2022.  Anysolo LLC
+ ******************************************************************************/
+
+package level2v3.unit5.examples
+
+
+data class SomeObject(val x: Int, val y: Int)
+
+// Removing some object from a list.
+
+fun main() {
+    val objects = mutableListOf(
+        SomeObject(100, 100),
+        SomeObject(100, 200),
+        SomeObject(100, 400),
+        SomeObject(100, 500),
+        SomeObject(100, 600)
+    )
+    println(objects)
+
+    val iterator = objects.listIterator()
+    while (iterator.hasNext()) {
+        val item = iterator.next()
+
+        if(item.y >= 400) {
+            iterator.remove()
+        }
+    }
+
+    println(objects)
+}
