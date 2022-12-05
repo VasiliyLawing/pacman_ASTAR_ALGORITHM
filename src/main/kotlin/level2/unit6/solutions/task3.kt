0fb9fca20fc0b948809632221d991a510be2ce04
dek <d.kischukov@gmail.com>
2022-12-05 16:36:58 -0500

    .

--- src/main/kotlin/level2/unit6/solutions/task3.kt
+++ src/main/kotlin/level2/unit6/solutions/task3.kt
@@ -2,8 +2,6 @@
  * Copyright (c) 2022.  Anysolo LLC
  ******************************************************************************/
 
-// Use Left, Right and Space key
-
 package level2.unit6.solutions
 
 import prgb1.solutions.level2.unit6.task3.task3
